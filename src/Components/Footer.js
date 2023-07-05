2c3192071b7c8fb66237fe38238f4efc7a1bf5bb
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2023-07-05 09:03:20 +0300

    myPortfolio

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="text-white p-7 border-t-[1px]  border-slate-300">
-      <p className="text-center">Copyright ©2020 All rights reserved</p>
+      <p className="text-center">Copyright ©2023 All rights reserved</p>
     </div>
   );
 }
