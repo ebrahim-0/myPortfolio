47303fddd4d462b1458e05b76b631d753a00145a
Ebrahim Abd EL Razik <ebrahimabdelrazik2002@gmail.com>
2023-07-06 04:04:15 +0300

    myPortfolio

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,13 +8,24 @@ export default function About() {
         About Me
       </h2>
       <div
-        className="text-3xl p-7 mt-12 leading-loose"
+        className="text-3xl p-7 my-12 leading-loose "
         data-aos="zoom-in-right"
       >
         <p className="text-start md:text-center">
           I'm a Front-End Developer building responsive websites project.
           Proficient in HTML5, CSS3, Bootstrap, Tailwind css, SASS, JavaScript,
           React.js, Redux Toolkit, Next.js, Git and GitHub
+        </p>
+      </div>
+
+      <h2 className="font-bold text-4xl md:text-6xl text-center title">
+        Education
+      </h2>
+
+      <div className="text-3xl p-7 mt-12 leading-loose" data-aos="zoom-in-up">
+        <p className="text-start md:text-center">
+          Faculty Of Engineering - Computer and Communication Engineering
+          department – CCE Mansoura University
         </p>
       </div>
     </div>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,8 @@
 import movieIcon from "./Image/moveIcon.png";
 import weatherIcon from "./Image/weatherIcon.png";
 import quizIcon from "./Image/quizIcon.png";
+import { Link } from "react-router-dom";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Projects() {
   return (
@@ -20,13 +22,13 @@ export default function Projects() {
 
           <div>
             <h4 className="py-5">Movies App</h4>
-            <a
+            <Link
               target="_blank"
-              href="https://react-movies-app100.netlify.app/"
-              className="px-3 py-1 rounded-xl text-lg transition-all duration-300 hover:underline button"
+              to="https://react-movies-app100.netlify.app/"
+              className="px-3 py-2 rounded-xl text-lg transition-all duration-300 hover:underline button"
             >
               Live Demo
-            </a>
+            </Link>
           </div>
         </div>
         <div
@@ -37,13 +39,13 @@ export default function Projects() {
 
           <div>
             <h4 className="py-5">Weather App</h4>
-            <a
+            <Link
               target="_blank"
-              href="https://react-weather-app100.netlify.app/"
-              className="px-3 py-1 rounded-xl transition-all duration-300 hover:underline button"
+              to="https://react-weather-app100.netlify.app/"
+              className="px-3 py-2 rounded-xl transition-all duration-300 hover:underline button"
             >
               Live Demo
-            </a>
+            </Link>
           </div>
         </div>
         <div
@@ -54,13 +56,30 @@ export default function Projects() {
 
           <div>
             <h4 className="py-5"> Quiz App</h4>
-            <a
+            <Link
               target="_blank"
-              href="https://quiz-app100.netlify.app/"
-              className="px-3 py-1 rounded-xl transition-all duration-300 hover:underline button"
+              to="https://quiz-app100.netlify.app/"
+              className="px-3 py-2 rounded-xl transition-all duration-300 hover:underline button"
             >
               Live Demo
-            </a>
+            </Link>
+          </div>
+        </div>
+        <div
+          className="card flex items-center gap-8  mt-8 p-5 text-xl"
+          data-aos="zoom-out-up"
+        >
+          <AiFillGithub className="w-28 h-28" />
+
+          <div>
+            <h4 className="py-5">Github Repo</h4>
+            <Link
+              target="_blank"
+              to="https://ebrahim-0.github.io/github-repos/"
+              className="px-3 py-2 rounded-xl transition-all duration-300 hover:underline button"
+            >
+              Live Demo
+            </Link>
           </div>
         </div>
       </div>
